f263121626cbd5b30fb5f01ce6709dc55e395ce8
Florin Salasan <florinsalasan@gmail.com>
2024-02-18 18:19:14 -0500

    add some more plugins, going to switch back to kickstart to have autoformat on save back though

--- lua/florinsalasan/lazy/telescope.lua
+++ lua/florinsalasan/lazy/telescope.lua
@@ -2,14 +2,14 @@ return {
     'nvim-telescope/telescope.nvim',
 
     dependencies = {
-        'nvim-lua/plenary.nvim' 
+        'nvim-lua/plenary.nvim'
     },
 
     config = function()
         require('telescope').setup({})
 
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>pws',function()
             local word = vim.fn.expand('<cword>')
